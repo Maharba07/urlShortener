f837e35d87bf8bcd722a2606c2fe364d001afd5d
Maharba07 <maharba1707@gmail.com>
2024-07-09 20:32:45 -0400

    deploy

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,13 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  user: process.env.DB_USER,        // 'tu_usuario_de_base_de_datos'
-  host: process.env.DB_HOST,        // 'localhost' o tu host de base de datos
-  database: process.env.DB_NAME,    // 'tu_nombre_de_base_de_datos'
-  password: process.env.DB_PASSWORD,// 'tu_contraseña_de_base_de_datos'
-  port: parseInt(process.env.DB_PORT || '5432'), // '5432' o el puerto de tu base de datos
+  user: process.env.DB_USER,        
+  host: process.env.DB_HOST,        
+  database: process.env.DB_NAME,    
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
+
 
 export default pool;
